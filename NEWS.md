3acc770bec580411010da33de984d721e9953d95
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2020-05-11 10:52:43 +0200

    Add compareInstalledVersions()
    The code has been taken from sema.scripts

--- NEWS.md
+++ NEWS.md
@@ -1,7 +1,8 @@
 # Current developments
 
 * move kwb.utils from Suggests to Imports
-* add installedDependencies() # 2020-04-28
+* add installedDependencies(), 2020-04-28
+* add compareInstalledVersions(), 2020-05-11
 
 # kwb.package 0.2.0
 
---
+++ R/compareInstalledVersions.R
@@ -0,0 +1,34 @@
+# compareInstalledVersions -----------------------------------------------------
+
+#' Compare Package Versions Between Libraries
+#' 
+#' @param lib1 path to first R library
+#' @param lib2 path to second R library
+#' @return data frame with columns \code{name} (package name), \code{version.1},
+#'   \code{version.2} (version string of package in \code{lib1} and \code{lib2},
+#'   respectively), \code{differs} (logical indicating whether the version is
+#'   different in the two libraries)
+#' @export
+compareInstalledVersions <- function(lib1, lib2)
+{
+  installed_versions <- function(x) {
+    versions <- utils::installed.packages(path.expand(x))
+    kwb.utils::resetRowNames(kwb.utils::noFactorDataFrame(
+      name = rownames(versions), 
+      version = versions[, "Version", drop = FALSE]
+    ))
+  }
+  
+  versions <- merge(
+    x = installed_versions(lib1), 
+    y = installed_versions(lib2), 
+    by = "name", 
+    all = TRUE
+  )
+  
+  names(versions)[-1L] <- c("version.1", "version.2")
+  
+  versions$differs <- (versions[[2L]] != versions[[3L]])
+  
+  versions[order(versions$name), , drop = FALSE]
+}
