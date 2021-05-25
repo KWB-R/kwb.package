3877fa9134b48a2bc2d0f837b3325cf7be272f16
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2021-05-26 01:54:33 +0200

    Add argument "ref_date", convert to Date

--- R/archived_cran_versions.R
+++ R/archived_cran_versions.R
@@ -1,6 +1,7 @@
 # archived_cran_versions -------------------------------------------------------
 # archived_cran_versions(c("ggplot2", "swmmr", "kwb.hantush"))
-archived_cran_versions <- function(package)
+# archived_cran_versions(c("ggplot2", "swmmr", "kwb.hantush"), ref_date= "2012-12-01")
+archived_cran_versions <- function(package, ref_date = NULL)
 {
   archive_url <- function(package) paste0(
     "https://cran.r-project.org/src/contrib/Archive/", package
@@ -24,9 +25,27 @@ archived_cran_versions <- function(package)
   
   pattern <- version_date_pattern(package)
   
-  cbind(package = package, kwb.utils::extractSubstring(
+  version_dates <- cbind(package = package, kwb.utils::extractSubstring(
     pattern = pattern,
     x = grep(pattern, html, value = TRUE), 
     index = c(version = 1L, date = 2L)
   ))
+
+  version_dates$date <- as.Date(version_dates$date)
+  
+  if (is.null(ref_date)) {
+    return(version_dates)
+  }
+  
+  get_last_version_before(version_dates, as.Date(ref_date))
+}
+
+# get_last_version_before ------------------------------------------------------
+get_last_version_before <- function(version_dates, ref_date)
+{
+  X = unname(split(version_dates, version_dates$package))
+  
+  last_before <- function(x) utils::tail(x[x$date <= ref_date, ], 1L)
+  
+  kwb.utils::resetRowNames(do.call(rbind, lapply(X, last_before)))
 }
