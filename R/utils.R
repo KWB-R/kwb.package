04fbb47bc137cc4c470780d6d2de4012d46aabb4
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2021-05-29 23:49:45 +0200

    Add utility functions
    - getUrl()
    - githubRepo()
    - readLinesFromUrl()
    - stop_()

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,25 @@
+# getUrl -----------------------------------------------------------------------
+getUrl <- function(key, ...)
+{
+  urls <- kwb.utils::resolve(..., x = list(
+    cran = "https://cran.r-project.org",
+    cran_contrib = "<cran>/src/contrib",
+    cran_package = "<cran>/web/packages/<package>",
+    cran_archive = "<cran_contrib>/Archive/<package>",
+    cran_archive_file = "<cran_archive>/<package_filename>",
+    cran_package_file = "<cran_contrib>/<package_filename>",
+    package_filename = "<package>_<version>.tar.gz"
+  ))
+  
+  kwb.utils::selectElements(urls, key)
+}
+
+# githubRepo -------------------------------------------------------------------
+githubRepo <- function(github_user, name)
+{
+  paste(github_user, name, sep = "/")
+}
+
 # packageInDestdir -------------------------------------------------------------
 packageInDestdir <- function(package, destdir, verbose = TRUE)
 {
@@ -10,6 +32,24 @@ packageInDestdir <- function(package, destdir, verbose = TRUE)
   }
   
   structure(file_exists, path = if (file_exists) kwb.utils::lastElement(files))
+}
+
+# readLinesFromUrl -------------------------------------------------------------
+readLinesFromUrl <- function(url, silent = TRUE)
+{
+  result <- try(suppressWarnings(readLines(url)), silent = silent)
+  
+  if (inherits(result, "try-error")) {
+    return(NULL)
+  }
+  
+  result
+}
+
+# stop_ ------------------------------------------------------------------------
+stop_ <- function(...)
+{
+  stop(..., call. = FALSE)
 }
 
 # dirPackageZips ---------------------------------------------------------------
