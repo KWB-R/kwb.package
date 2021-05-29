9ac0c848c87395ae5002ba0e568ea57bd7e32a94
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2021-05-29 12:12:36 +0200

    Rename files according to function names

--- R/githubPackageVersions.R
+++ R/githubPackageVersions.R
@@ -1,49 +1,54 @@
 # githubPackageVersions --------------------------------------------------------
+# Haukes version of github_packages_versions()
 githubPackageVersions <- function(
-  repo, 
+  repo,
   auth_token = remotes:::github_pat(),
   verbose = TRUE
 )
 {
   stopifnot(is.character(repo))
-  
+
   if (length(repo) > 1L) {
     return(do.call(rbind, lapply(
-      repo, 
-      githubPackageVersions, 
-      auth_token = auth_token, 
+      repo,
+      githubPackageVersions,
+      auth_token = auth_token,
       verbose = verbose
     )))
   }
-  
+
   if (verbose) {
     message("Reading ", repo)
   }
 
   # Shortcut
   get <- kwb.utils::selectColumns
-  
+
+  # Endpoint to raw DESCRIPTION file of a certain commit
   description_url <- function(repo, sha) sprintf(
     "https://raw.githubusercontent.com/%s/%s/DESCRIPTION", repo, sha
   )
-  
+
+  # Get release information (may be NULL if there are no releases)
   result <- getGithubReleaseInfo(repo, reduced = FALSE, auth_token = auth_token)
 
+  # Return NULL if there are no releases
   if (is.null(result)) {
     return(NULL)
   }
-  
+
+  # Read the description files of the commits referred to by the releases
   descriptions <- lapply(
-    get(result, "sha"), 
+    get(result, "sha"),
     readGithubPackageDescription,
     repo = repo,
     auth_token = auth_token
   )
-  
+
   result$package <- basename(result$repo)
-  
+
   result$remote <- sprintf("github::%s@%s", result$repo, result$tag)
-  
+
   result$version <- sapply(descriptions, function(x) {
     if (! "Version" %in% colnames(x)) {
       return(NA_character_)
@@ -52,10 +57,10 @@ githubPackageVersions <- function(
   })
 
   result <- kwb.utils::removeColumns(result, c("sha", "repo", "tag", "release"))
-  result <- kwb.utils::moveColumnsToFront(result,c("package", "version", "date"))
-  
+  result <- kwb.utils::moveColumnsToFront(result, c("package", "version", "date"))
+
   result <- result[! is.na(result$date) & ! is.na(result$version), ]
-  
+
   kwb.utils::orderBy(result, "date")
 }
 
@@ -66,7 +71,7 @@ getGithubReleaseInfo <- function(
 {
   # Shortcut
   get <- kwb.utils::selectElements
-  
+
   releases_url <- function(repo) sprintf(
     "https://api.github.com/repos/%s/releases", repo
   )
@@ -79,36 +84,36 @@ getGithubReleaseInfo <- function(
     stopifnot(length(endpoint) == 1L)
     gh::gh(endpoint, .token = auth_token)
   }
-  
+
   releases <- get_endpoint(endpoint = releases_url(repo))
   tags <- get_endpoint(endpoint = tags_url(repo))
 
   if (length(tags) == 0L) {
     return(NULL)
   }
-  
+
   tag_info <- kwb.utils::noFactorDataFrame(
     tag = sapply(tags, get, "name"),
     sha = sapply(lapply(tags, get, "commit"), get, "sha")
   )
-  
+
   release_info <- kwb.utils::noFactorDataFrame(
     tag = sapply(releases, get, "tag_name"),
     date = as.Date(sapply(releases, get, "published_at")),
     release = sapply(releases, get, "name"),
     author = sapply(releases, function(x) get(get(x, "author"), "login"))
   )
-  
+
   result <- cbind(
     repo = repo,
     merge(tag_info, release_info, by = "tag", all.x = TRUE),
     stringsAsFactors = FALSE
   )
-  
+
   if (! reduced) {
     return(result)
   }
-  
+
   kwb.utils::removeColumns(result, "sha")
 }
 
@@ -127,9 +132,9 @@ readGithubPackageDescription <- function(
   if (inherits(content, "try-error")) {
     return(NULL)
   }
-  
+
   con <- textConnection(kwb.utils::selectElements(content, "message"))
   on.exit(close(con))
-  
+
   read.dcf(con)
 }
