ec976630c854a22e87398299f186d0eba158acb9
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2021-05-29 23:54:50 +0200

    Add cranVersions(), isOnCran()
    - Add currentCranVersion()
    - Improve archivedCranVersions()

--- R/archivedCranVersions.R
+++ R/archivedCranVersions.R
@@ -1,45 +1,107 @@
-# archivedCranVersions ---------------------------------------------------------
-# archivedCranVersions(c("ggplot2", "swmmr", "kwb.hantush"))
-# archivedCranVersions(c("ggplot2", "swmmr", "kwb.hantush"), ref_date= "2012-12-01")
-archivedCranVersions <- function(package, ref_date = NULL)
+# isOnCran ---------------------------------------------------------------------
+isOnCran <- function(name)
 {
-  archive_url <- function(package) paste0(
-    "https://cran.r-project.org/src/contrib/Archive/", package
+  nrow(currentCranVersion(name)) > 0L
+}
+
+# cranVersions -----------------------------------------------------------------
+cranVersions <- function(name)
+{
+  current <- currentCranVersion(name)
+    
+  if (nrow(current) == 0L) {
+    message(sprintf("Package '%s' does not seem to be on CRAN.", name))
+    return(NULL)
+  }
+
+  archived <- archivedCranVersions(name)
+
+  current$package_source_url <- getUrl(
+    "cran_package_file", package = name, version = current$version
   )
   
-  version_date_pattern <- function(package) sprintf(
-    "href=\"%s_(.*)\\.tar\\.gz\".*(\\d{4}-\\d{2}-\\d{2}) ", package
+  archived$package_source_url <- sprintf(
+    getUrl("cran_archive_file", package = name, package_filename = "%s"), 
+    archived$archive_file
   )
+  
+  result <- kwb.utils::safeRowBind(archived, current)
+  
+  kwb.utils::removeColumns(result, "archive_file")
+}
 
+# currentCranVersion -----------------------------------------------------------
+currentCranVersion <- function(name)
+{
+  src <- readLinesFromUrl(getUrl("cran_package", package = name))
+  
+  if (is.null(src)) {
+    return(kwb.utils::noFactorDataFrame(
+      package = character(0L),
+      version = character(0L),
+      date = as.Date(character(0L)),
+      date_type = character(0L)
+    ))
+  }
+  
+  extract <- function(x) {
+    gsub("<td>|</td>", "", src[grep(sprintf("<td>%s:</td>", x), src) + 1L])
+  }
+  
+  kwb.utils::noFactorDataFrame(
+    package = name,
+    version = extract("Version"),
+    date = as.Date(extract("Published")),
+    date_type = "published"
+  )
+}
+
+# archivedCranVersions ---------------------------------------------------------
+# packages <- c("ggplot2", "swmmr", "kwb.hantush")
+# archivedCranVersions(packages)
+# archivedCranVersions(packages, ref_date= "2012-12-01")
+archivedCranVersions <- function(package, ref_date = NULL)
+{
   if (length(package) > 1L) {
     return(do.call(rbind, lapply(
       package, archivedCranVersions, ref_date = ref_date
     )))
   } 
   
-  url <- archive_url(package)
+  src <- readLinesFromUrl(getUrl("cran_archive", package = package))
 
-  html <- suppressWarnings(try(readLines(url), silent = TRUE))
-
-  if (inherits(html, "try-error")) {
-    return(data.frame(version = character(), date = character()))
+  if (is.null(src)) {
+    return(kwb.utils::noFactorDataFrame(
+      package = character(0L),
+      version = character(0L),
+      date = as.Date(character(0L)),
+      archive_file = character(0L),
+      date_type = character(0L)
+    ))
   }
   
-  pattern <- version_date_pattern(package)
+  pattern <- sprintf(
+    "href=\"(%s_(.*)\\.tar\\.gz)\".*(\\d{4}-\\d{2}-\\d{2}) ", package
+  )
   
-  version_dates <- cbind(package = package, kwb.utils::extractSubstring(
+  versions <- cbind(package = package, kwb.utils::extractSubstring(
     pattern = pattern,
-    x = grep(pattern, html, value = TRUE), 
-    index = c(version = 1L, date = 2L)
+    x = grep(pattern, src, value = TRUE), 
+    index = c(
+      version = 2L, 
+      date = 3L, 
+      archive_file = 1L
+    )
   ))
 
-  version_dates$date <- as.Date(version_dates$date)
+  versions$date <- as.Date(versions$date)
+  versions$date_type <- "last_modified"
   
   if (is.null(ref_date)) {
-    return(version_dates)
+    return(versions)
   }
   
-  getLastVersionBefore(version_dates, as.Date(ref_date))
+  getLastVersionBefore(versions, as.Date(ref_date))
 }
 
 # getLastVersionBefore ---------------------------------------------------------
