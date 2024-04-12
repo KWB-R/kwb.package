e3bce7f1c97e992aabd2d3da9a28e0d10eb0bfaa
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2024-04-12 17:55:38 +0200

    Add getPackageLicences()

---
+++ R/getPackageLicences.R
@@ -0,0 +1,27 @@
+# getPackageLicences -----------------------------------------------------------
+
+#' Which Licences are Specified for the Packages?
+#' 
+#' @param packages names of (installed) packages
+#' @return data frame
+#' @export
+getPackageLicences <- function(packages)
+{
+  lapply(packages, function(package) {
+    description <- readDescription(package)
+    fields <- c("licence", "license")
+    columns <- intersect(colnames(description), fields)
+    as.data.frame(description[, columns, drop = FALSE])
+  }) %>%
+    stats::setNames(packages) %>%
+    rbindAll(nameColumn = "package") %>% 
+    moveColumnsToFront("package")
+}
+
+# readDescription --------------------------------------------------------------
+readDescription <- function(package)
+{
+  description <- read.dcf(system.file("DESCRIPTION", package = package))
+  colnames(description) <- tolower(colnames(description))
+  description
+}
