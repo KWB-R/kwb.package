82e10f17ddb51cc86591c09c88f7e1cd02999da2
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2024-04-16 16:12:05 +0200

    Do not convert "package" column to a factor

--- R/getPackageLicences.R
+++ R/getPackageLicences.R
@@ -4,6 +4,7 @@
 #' 
 #' @param packages names of (installed) packages
 #' @return data frame
+#' @importFrom kwb.utils moveColumnsToFront rbindAll
 #' @export
 getPackageLicences <- function(packages)
 {
@@ -14,8 +15,8 @@ getPackageLicences <- function(packages)
     as.data.frame(description[, columns, drop = FALSE])
   }) %>%
     stats::setNames(packages) %>%
-    rbindAll(nameColumn = "package") %>% 
-    moveColumnsToFront("package")
+    kwb.utils::rbindAll(nameColumn = "package", namesAsFactor = FALSE) %>% 
+    kwb.utils::moveColumnsToFront("package")
 }
 
 # readDescription --------------------------------------------------------------
