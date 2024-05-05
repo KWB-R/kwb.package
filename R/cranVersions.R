4e3688e0816a71f5c23dc8db03d972e5c31affd0
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2024-05-05 18:44:44 +0200

    Integrate URL pattern in sprintf() call
    and use selectColumns()

--- R/cranVersions.R
+++ R/cranVersions.R
@@ -20,18 +20,16 @@ cranVersions <- function(name, dbg = TRUE)
   current$package_source_url <- getUrl(
     "cran_package_file", 
     package = name, 
-    version = current$version
+    version = selectColumns(current, "version")
   )
   
-  urlPattern <- getUrl(
-    "cran_archive_file", 
-    package = name, 
-    package_filename = "%s"
+  archived$package_source_url <- sprintf(
+    getUrl("cran_archive_file", package = name, package_filename = "%s"), 
+    selectColumns(archived, "archive_file")
   )
-  
-  archived$package_source_url <- sprintf(urlPattern, archived$archive_file)
   
   result <- safeRowBind(archived, current)
   
   removeColumns(result, "archive_file")
 }
+
